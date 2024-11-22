0b1caab69bdcfe72792321b214fbe5537bb9c79f
Tim Hechenberger <timhec44@gmail.com>
2024-11-22 14:13:23 +0100

    Initial Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'mydb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
 })
+
 export class AppModule {}
