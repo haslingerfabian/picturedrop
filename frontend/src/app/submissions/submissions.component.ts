94027bcd86d4dfd5e45c920aa4ccdc84b5aacd33
Fabian <haslinger.fabian1503@gmail.com>
2025-03-28 14:22:35 +0100

    Add submissions component with template, styles, and tests

---
+++ frontend/src/app/submissions/submissions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submissions',
+  imports: [],
+  templateUrl: './submissions.component.html',
+  styleUrl: './submissions.component.css'
+})
+export class SubmissionsComponent {
+
+}
