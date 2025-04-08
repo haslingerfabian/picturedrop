44e4bb054bc4f4a3d5ba63b7bd7b984f8a6aa97c
Fabian <haslinger.fabian1503@gmail.com>
2025-04-08 13:21:42 +0200

    Stable version

--- frontend/src/app/submissions/submissions.component.ts
+++ frontend/src/app/submissions/submissions.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-submissions',
