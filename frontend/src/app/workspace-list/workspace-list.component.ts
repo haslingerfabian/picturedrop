4eee882a0f9e3ebd88f6050073bdab949ce2ebd3
Fabian <haslinger.fabian1503@gmail.com>
2025-04-08 13:48:00 +0200

    Refactor workspaces component: replace submissions with workspaces, update styles, and add sample data

---
+++ frontend/src/app/workspace-list/workspace-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-list',
+  imports: [],
+  templateUrl: './workspace-list.component.html',
+  styleUrl: './workspace-list.component.css'
+})
+export class WorkspaceListComponent {
+
+}
--- frontend/src/app/workspaces/workspaces.component.ts
+++ frontend/src/app/workspaces/workspaces.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-workspaces',
+  imports: [CommonModule],
   templateUrl: './workspaces.component.html',
   styleUrls: ['./workspaces.component.scss']
 })
 export class WorkspacesComponent {
-  // Hier können später Daten für die Workspaces und Statistiken hinzugefügt werden
+  items = Array.from({ length: 20 }, (_, i) => `Element ${i + 1}`); // Beispiel-Liste
 }
