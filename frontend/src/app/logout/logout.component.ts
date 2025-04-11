c32b35ab60d2fc7ca4b5bb0ddf26357a31ec31eb
Tim Hechenberger <timhec44@gmail.com>
2025-04-11 11:09:37 +0200

    Add Logout component with styling and integration into AppComponent

---
+++ frontend/src/app/logout/logout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoComponent } from "../logo/logo.component";
+
+@Component({
+  selector: 'app-logout',
+  imports: [LogoComponent],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.scss'
+})
+export class LogoutComponent {
+
+}
